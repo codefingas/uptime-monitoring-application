7f8ab330554454f141cd2061a04a13e0a25583f1
unknown <udohezekiel34@yahoo.com>
2020-06-16 06:36:55 +0100

    scaffolded app, created server, parsing req.url

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const url = require('url');
+
+
+const server = http.createServer((req, res) => {
+    const parseUrl = url.parse(req.url, true); //parses the url
+
+    let path = parseUrl.pathname;
+    let trimmedUrl = path.replace(/^\/+|\/+$/g, '')// the empty string trims off extra slashes that the user might send along with his request
+
+    res.end('HELLO WORLD!'); //returns hello world to client
+
+    console.log("REQUEST RECIEVED ON " + trimmedUrl);// logging the recieved path
+});
+
+server.listen(5000, console.log("SERVER IS LISTENING ON PORT 5000"));
