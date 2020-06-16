0a2350e8b6c1db1df00bcdedbc8282cd88d53a3c
unknown <udohezekiel34@yahoo.com>
2020-06-16 06:45:45 +0100

    parsing http method

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ const server = http.createServer((req, res) => {
 
     let path = parseUrl.pathname;
     let trimmedUrl = path.replace(/^\/+|\/+$/g, '')// the empty string trims off extra slashes that the user might send along with his request
+    let method = req.method.toLowerCase();
 
     res.end('HELLO WORLD!'); //returns hello world to client
 
-    console.log("REQUEST RECIEVED ON " + trimmedUrl);// logging the recieved path
+    console.log("REQUEST RECIEVED ON " + trimmedUrl + " WITH THIS METHOD " + method);// logging the recieved path
 });
 
 server.listen(5000, console.log("SERVER IS LISTENING ON PORT 5000"));
